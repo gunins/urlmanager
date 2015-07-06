b8d1c0f92ad1d2364a20ce316677f1c9649f5a8e
Guntars Simanskis <guntars@diamondgraphics.ie>
2015-07-06 12:15:56 +0100

    Release v0.2.11

--- dist/router/MatchBinding.js
+++ dist/router/MatchBinding.js
@@ -10,7 +10,7 @@
         module.exports = factory();
     } else {
         // Browser globals (root is window)
-        root.UrlManager = root.UrlManager || {};
+        root.UrlManager              = root.UrlManager || {};
         root.UrlManager.MatchBinding = factory();
     }
 }(this, function () {
@@ -19,29 +19,29 @@
             this.pattern = location = pattern.replace(/^\(\/\)/g, '').replace(/^\/|$/g, '');
         } else {
             this.pattern = pattern;
-            location = (location + pattern);
+            location     = (location + pattern);
         }
         this.location = location.replace(/\((.*?)\)/g, '$1').replace(/^\/|$/g, '');
 
         var route = this.pattern.replace(MatchBinding.ESCAPE_PARAM, '\\$&')
             .replace(MatchBinding.OPTIONAL_PARAM, '(?:$1)?')
             .replace(MatchBinding.NAMED_PARAM, function (match, optional) {
-                return optional ? match : '([^\/]+)';
-            }).replace(MatchBinding.SPLAT_PARAM, '(.*?)');
+                         return optional ? match : '([^\/]+)';
+                     }).replace(MatchBinding.SPLAT_PARAM, '(.*?)');
 
         this.patternRegExp = new RegExp('^' + route);
-        this.routeHandler = [];
-        this.leaveHandler = [];
-        this.queryHandler = [];
-        this.routes = [];
+        this.routeHandler  = [];
+        this.leaveHandler  = [];
+        this.queryHandler  = [];
+        this.routes        = [];
     }
 
-    MatchBinding.prototype.onBind = function () {
+    MatchBinding.prototype.onBind    = function () {
     };
     MatchBinding.prototype.setOnBind = function (onBinding) {
         this.onBind = onBinding
     };
-    MatchBinding.prototype.rebind = function () {
+    MatchBinding.prototype.rebind    = function () {
         if (this.onBind !== undefined) {
             this.onBind();
         }
@@ -56,44 +56,51 @@
         return this.routes;
     };
 
-    MatchBinding.prototype.to = function (routeHandler) {
+    MatchBinding.prototype.to              = function (routeHandler) {
         this.routeHandler.push(routeHandler);
         return this;
     };
-    MatchBinding.prototype.leave = function (leaveHandler) {
+    MatchBinding.prototype.leave           = function (leaveHandler) {
         this.leaveHandler.push(leaveHandler);
         return this;
     };
-    MatchBinding.prototype.query = function (queryHandler) {
+    MatchBinding.prototype.query           = function (queryHandler) {
         this.queryHandler.push(queryHandler);
         return this;
     };
-    MatchBinding.prototype.test = function (location) {
+    MatchBinding.prototype.remove          = function () {
+        this.routes.splice(0, this.routes.length);
+        this.routeHandler.splice(0, this.routeHandler.length);
+        this.leaveHandler.splice(0, this.leaveHandler.length);
+        this.queryHandler.splice(0, this.queryHandler.length);
+        return this;
+    };
+    MatchBinding.prototype.test            = function (location) {
         return this.patternRegExp.test(location);
     };
-    MatchBinding.prototype.getFragment = function (location) {
+    MatchBinding.prototype.getFragment     = function (location) {
         var subLocation = this.applyParams(location);
         return location.replace(subLocation, '');
     };
-    MatchBinding.prototype.applyParams = function (location) {
-        var matches = this.pattern.replace(/\((.*?)\)/g, '$1').split('/');
+    MatchBinding.prototype.applyParams     = function (location) {
+        var matches  = this.pattern.replace(/\((.*?)\)/g, '$1').split('/');
         var matches2 = location.split('/');
         return matches2.splice(0, matches.length).join('/');
     };
-    MatchBinding.prototype.extractParams = function (fragment) {
+    MatchBinding.prototype.extractParams   = function (fragment) {
         var params = this.patternRegExp.exec(fragment).slice(1);
         return params.map(function (param) {
             return param ? decodeURIComponent(param) : null;
         });
     };
-    MatchBinding.prototype.setSubBinder = function (subBinder) {
+    MatchBinding.prototype.setSubBinder    = function (subBinder) {
         this.subBinder = subBinder;
         return subBinder;
     };
-    MatchBinding.prototype.getSubBinder = function () {
+    MatchBinding.prototype.getSubBinder    = function () {
         return this.subBinder;
     };
-    MatchBinding.prototype.getHandler = function () {
+    MatchBinding.prototype.getHandler      = function () {
         return this.routeHandler;
     };
     MatchBinding.prototype.getLeaveHandler = function () {
@@ -104,9 +111,9 @@
     };
 
     MatchBinding.OPTIONAL_PARAM = /\((.*?)\)/g;
-    MatchBinding.NAMED_PARAM = /(\(\?)?:\w+/g;
-    MatchBinding.SPLAT_PARAM = /\*\w+/g;
-    MatchBinding.ESCAPE_PARAM = /[\-{}\[\]+?.,\\\^$|#\s]/g;
+    MatchBinding.NAMED_PARAM    = /(\(\?)?:\w+/g;
+    MatchBinding.SPLAT_PARAM    = /\*\w+/g;
+    MatchBinding.ESCAPE_PARAM   = /[\-{}\[\]+?.,\\\^$|#\s]/g;
 
     return MatchBinding;
 }));
